14e1d15749e7294fc01167198a217fbb11405d7f
Johann T Mariusson <jtm@uc4.rhi.hi.is>
2010-11-11 13:01:54 +0000

    wrong boolean expression

--- Report.php
+++ Report.php
@@ -96,8 +96,7 @@ class Report {
 		}
 		if($this->options & OPT_VERBOSE) 
 			echo "Creating `$this->output_dir`\n";
-		if(file_exists($this->output_dir) 
-			|| !mkdir($this->output_dir, 0775)) {
+		if(!mkdir($this->output_dir, 0775)) {
 			die("Unable to create `$this->output_dir`...\n");
 		}
 		$this->output_dir = realpath($this->output_dir);
